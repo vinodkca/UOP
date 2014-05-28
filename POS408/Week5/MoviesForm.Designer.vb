0364817acb189e2810aa0d0840c2fe9ac60b10e0
vinodkca <vinodkca@lycos.com>
2014-05-27 22:02:27 -0700

    added movies form

---
+++ POS408/Week5/MoviesForm.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MoviesForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.dgrMovies = New System.Windows.Forms.DataGridView()
+        Me.btnFill = New System.Windows.Forms.Button()
+        Me.btnSorted = New System.Windows.Forms.Button()
+        Me.btnStartsA = New System.Windows.Forms.Button()
+        CType(Me.dgrMovies, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dgrMovies
+        '
+        Me.dgrMovies.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgrMovies.Location = New System.Drawing.Point(36, 41)
+        Me.dgrMovies.Name = "dgrMovies"
+        Me.dgrMovies.Size = New System.Drawing.Size(591, 150)
+        Me.dgrMovies.TabIndex = 0
+        '
+        'btnFill
+        '
+        Me.btnFill.Location = New System.Drawing.Point(36, 219)
+        Me.btnFill.Name = "btnFill"
+        Me.btnFill.Size = New System.Drawing.Size(75, 23)
+        Me.btnFill.TabIndex = 1
+        Me.btnFill.Text = "Fill"
+        Me.btnFill.UseVisualStyleBackColor = True
+        '
+        'btnSorted
+        '
+        Me.btnSorted.Location = New System.Drawing.Point(303, 219)
+        Me.btnSorted.Name = "btnSorted"
+        Me.btnSorted.Size = New System.Drawing.Size(75, 23)
+        Me.btnSorted.TabIndex = 2
+        Me.btnSorted.Text = "Sorted By Year"
+        Me.btnSorted.UseVisualStyleBackColor = True
+        '
+        'btnStartsA
+        '
+        Me.btnStartsA.Location = New System.Drawing.Point(163, 219)
+        Me.btnStartsA.Name = "btnStartsA"
+        Me.btnStartsA.Size = New System.Drawing.Size(75, 23)
+        Me.btnStartsA.TabIndex = 3
+        Me.btnStartsA.Text = "Starts with A"
+        Me.btnStartsA.UseVisualStyleBackColor = True
+        '
+        'MoviesForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(666, 262)
+        Me.Controls.Add(Me.btnStartsA)
+        Me.Controls.Add(Me.btnSorted)
+        Me.Controls.Add(Me.btnFill)
+        Me.Controls.Add(Me.dgrMovies)
+        Me.Name = "MoviesForm"
+        Me.Text = "Movies Form"
+        CType(Me.dgrMovies, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents dgrMovies As System.Windows.Forms.DataGridView
+    Friend WithEvents btnFill As System.Windows.Forms.Button
+    Friend WithEvents btnSorted As System.Windows.Forms.Button
+    Friend WithEvents btnStartsA As System.Windows.Forms.Button
+End Class
---
+++ POS408/Week5/MoviesForm.vb
@@ -0,0 +1,50 @@
+﻿Public Class MoviesForm
+
+    Private Sub MoviesForm_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        dgrMovies.AllowUserToAddRows = False
+    End Sub
+
+    Private Sub btnFill_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnFill.Click
+        SampleFill()
+    End Sub
+
+    Private Sub btnStartsA_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnStartsA.Click
+        SampleStartWithA()
+    End Sub
+
+    Private Sub btnSorted_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSorted.Click
+        SamplSorted()
+    End Sub
+
+    Private Sub SampleFill()
+        Dim dc As New LINQMoviesDataContext()
+        Dim q = _
+            From a In dc.GetTable(Of Movie)() _
+            Select a
+        dgrMovies.DataSource = q
+    End Sub
+
+    Private Sub SampleStartWithA()
+        Dim dc As New LINQMoviesDataContext()
+        Dim q = _
+            From a In dc.GetTable(Of Movie)() _
+            Where a.MovieName.StartsWith("A") _
+            Select a
+        dgrMovies.DataSource = q
+    End Sub
+
+    Private Sub SamplSorted()
+        Dim dc As New LINQMoviesDataContext()
+        Dim q = _
+            From a In dc.GetTable(Of Movie)()
+            Order By a.YearPublished Ascending
+            Select a
+        dgrMovies.DataSource = q
+    End Sub
+
+    Private Sub dgrMovies_CellClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles dgrMovies.CellClick
+        For Each dc As DataGridViewColumn In dgrMovies.Columns
+            dc.ReadOnly = True
+        Next
+    End Sub
+End Class
